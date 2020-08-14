ae2bef845b693f1fabcf088db088c212359cd967
Demon000 <demonsingur@gmail.com>
2020-08-15 01:52:22 +0300

    joyeuse: Add device specific init library

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -15,5 +15,9 @@ DEVICE_PATH := device/xiaomi/joyeuse
 # Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := joyeuse
 
+# Init
+TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_miatoll
+TARGET_RECOVERY_DEVICE_MODULES := libinit_miatoll
+
 # OTA assert
 TARGET_OTA_ASSERT_DEVICE := joyeuse
