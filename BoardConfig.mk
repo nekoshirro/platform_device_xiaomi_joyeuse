a262156f2528d33454c7790d12efb08d2ccec2a2
nekoskuyy <ais.muzakky@gmail.com>
2022-04-11 19:42:55 +0700

    joyeuse: Switch device init library to spesific joyeuse
    Signed-off-by: nekoskuyy <ais.muzakky@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -16,8 +16,8 @@ DEVICE_PATH := device/xiaomi/joyeuse
 TARGET_BOOTLOADER_BOARD_NAME := joyeuse
 
 # Init
-TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_miatoll
-TARGET_RECOVERY_DEVICE_MODULES := libinit_miatoll
+TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_joyeuse
+TARGET_RECOVERY_DEVICE_MODULES := libinit_joyeuse
 
 # OTA assert
 TARGET_OTA_ASSERT_DEVICE := joyeuse
